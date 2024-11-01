707ad5ce82af9a1dfad37c5899a1f971551fff99
Maciej Bartczak <39600846+maciekbartczak@users.noreply.github.com>
2024-11-01 15:02:53 +0100

    implement a simple runner to test programs

---
+++ tests/runner.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::{BufRead, Read};
+use std::process::Command;
+use std::{env, fs};
+
+fn main() {
+    let mut run_build = true;
+
+    let args: Vec<String> = env::args().collect();
+    if let Some(flag) = try_consume_flag(args) {
+        match flag.as_str() {
+            "skipBuild" => run_build = false,
+            _ => panic!("Unsupported flag provided: {}", flag),
+        }
+    }
+
+    let programs = get_files_with_extension("./tests/programs", "yep");
+
+    for program in programs {
+        // read the expected result
+        let result_filename = program.replace(".yep", ".result");
+        let mut result_file = File::open(&result_filename)
+            .expect(format!("Cannot open {}, make sure it exists.", result_filename).as_str());
+
+        let mut expected_contents = String::new();
+        result_file.read_to_string(&mut expected_contents).unwrap();
+
+        let expected_lines: Vec<String> =
+            expected_contents.lines().map(|s| s.to_string()).collect();
+
+        // compile the program
+        Command::new("target/debug/yep")
+            .arg(format!("{}", program))
+            .output()
+            .expect("failed to execute yep");
+
+        // TEMP: This eventually will be integrated into the yep executable
+        Command::new("bash")
+            .arg("./compile.sh")
+            .output()
+            .expect("failed to execute compile.sh");
+
+        let program_output = Command::new("./program")
+            .output()
+            .expect("failed to execute program");
+
+        let std_output = String::from_utf8(program_output.stdout).unwrap();
+
+        let program_lines: Vec<String> = std_output.lines().map(String::from).collect();
+
+        print!(
+            "{}... {}\n",
+            program,
+            if program_lines == expected_lines {
+                "OK"
+            } else {
+                "FAIL"
+            }
+        );
+    }
+}
+fn get_files_with_extension(directory: &str, extension: &str) -> Vec<String> {
+    match fs::read_dir(directory) {
+        Ok(entries) => entries
+            .filter_map(|entry| {
+                entry.ok().and_then(|dir_entry| {
+                    let path = dir_entry.path();
+
+                    if path.is_file() && path.extension().map_or(false, |ext| ext == extension) {
+                        path.to_str().map(|s| s.to_string())
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect(),
+        Err(e) => {
+            eprintln!("Error reading directory: {}", e);
+            Vec::new()
+        }
+    }
+}
+
+fn try_consume_flag(args: Vec<String>) -> Option<String> {
+    if args.len() == 1 {
+        return None;
+    }
+
+    let flag = &args[1];
+    if !flag.starts_with("--") {
+        panic!("Expected flag to start with '--'");
+    }
+
+    let value = &flag[2..];
+    Some(value.to_string())
+}
