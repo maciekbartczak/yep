abec91f2eb6fe4572be1424bdb55dbbcf6553c86
Maciej Bartczak <39600846+maciekbartczak@users.noreply.github.com>
2024-11-03 11:52:20 +0100

    add support for custom output filename

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::fs::File;
 
 use parser::Parser;
 use std::env;
+use std::env::Args;
 use std::fs;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use tokenizer::Tokenizer;
 
@@ -15,35 +16,78 @@ mod partial_evaluator;
 mod remove_complex_operands;
 mod tokenizer;
 
+struct CompileOptions {
+    source_path: PathBuf,
+    output_path: PathBuf,
+    compile_runtime: bool
+}
+
+impl From<Args> for CompileOptions {
+    fn from(value: Args) -> Self {
+        let args: Vec<String> =  value.collect();
+
+        if args.len() < 2 {
+            eprintln!("Usage: yep <filename> [-o <output_filename>]");
+            panic!();
+        };
+
+        let mut args = args.into_iter().skip(1).peekable();
+        let source_path = PathBuf::from(args.next().unwrap().as_str());
+        if !source_path.is_file() {
+            panic!("Only files can be compiled");
+        };
+
+        let output_path = if let Some(flag) = args.next() {
+            if flag == "-o" {
+                if args.peek().is_none() {
+                    eprintln!("-o flag provided with no value");
+                    panic!();
+                }
+
+                PathBuf::from(args.next().unwrap().as_str())
+            } else {
+                eprintln!("Unknown flag provided: {}", flag);
+                panic!();
+            }
+        } else {
+            source_path.with_extension("")
+        };
+
+        Self {
+            source_path,
+            output_path,
+            compile_runtime: true
+        }
+    }
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: yep <filename>");
-        return;
-    }
+    let compile_options = CompileOptions::from(env::args());
 
-    let source_path = Path::new(&args[1]);
-    if !source_path.is_file() {
-        panic!("Only files can be compiled");
-    }
+    let source = fs::read_to_string(&compile_options.source_path).unwrap();
 
-    let source = fs::read_to_string(source_path).unwrap();
-    println!("Compiling {}", source_path.display());
-
+    println!("Compiling {}", compile_options.source_path.display());
     let tokens = Tokenizer::new(source).tokenize();
     let program = Parser::new(tokens).parse();
 
     let mut codegen = codegen::X86AssemblyCodegen::new(program);
     let instructions = codegen.generate();
 
-    let asm_path = source_path.with_extension("asm");
+    let asm_path = compile_options.output_path.with_extension("asm");
 
     let mut file = File::create(&asm_path).unwrap();
     for instruction in instructions {
         writeln!(file, "{}", instruction).unwrap();
     }
 
-    let object_path = source_path.with_extension("o");
+    if compile_options.compile_runtime {
+        Command::new("gcc")
+            .args(["-c", "runtime.c", "-o", "runtime.o"])
+            .spawn()
+            .expect("failed to compile runtime");
+    }
+
+    let object_path = compile_options.output_path.with_extension("o");
     Command::new("nasm")
         .args(vec![
             "-f",
@@ -55,8 +99,8 @@ fn main() {
         .output()
         .expect("failed to compile");
 
-    let program_path = source_path.with_extension("");
-    Command::new("gcc")
+    let program_path = compile_options.output_path;
+    let gcc_output = Command::new("gcc")
         .args(vec![
             &object_path.display().to_string(),
             "runtime.o",
@@ -65,4 +109,7 @@ fn main() {
         ])
         .output()
         .expect("failed to compile");
+
+    let stdout = String::from_utf8_lossy(&gcc_output.stdout);
+    let stderr = String::from_utf8_lossy(&gcc_output.stderr);
 }
